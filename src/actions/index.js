71ef667c034a0e7f700881ad52f66ab88723be4d
ellymaimon <eliranmaimon@gmail.com>
2018-09-25 10:58:43 -0700

    Add fetch song id function to handle API call and request song action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import * as types from './../constants/ActionTypes';
+import v4 from 'uuid/v4';
 
 export const nextLyric = (currentSongId) => ({
   type: types.NEXT_LYRIC,
@@ -14,3 +15,23 @@ export const changeSong = (newSelectedSongId) => ({
   type: types.CHANGE_SONG,
   newSelectedSongId
 });
+
+export const requestSong = (title, localSongId) => ({
+  type: types.REQUEST_SONG,
+  title,
+  songId: localSongId
+})
+
+export function fetchSongId(title) {
+  return function (dispatch) {
+    const localSongId = v4();
+    dispatch(requestSong(title, localSongId));
+    title = title.replace(' ', '_');
+    return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=ae75100d21ead154d0502672ad52890b').then(
+      response => response.json(),
+      error => console.log('An error occurred.', error)
+    ).then(function(json) {
+      console.log('CHECK OUT THIS SWEET API RESPONSE:', json)
+    });   
+  };
+}
